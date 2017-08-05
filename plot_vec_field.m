2f5af140b02dcdb4f96aafa99b7f3c64eb3903be
minjay <mjfan@ucar.edu>
2017-08-05 15:01:53 -0700

    Just draw one plot.

--- plot_vec_field.m
+++ plot_vec_field.m
@@ -35,30 +35,16 @@ M = size(A_2_1, 2);
 alpha = 3;
 sigma = B.^((-alpha/2)*(j_min:j_max));
 % highly correlated case
-C = [1, 0.9; 0.9, 1];
-nu = 3;
+C = [1, 0.5; 0.5, 1];
+nu = 2.5;
 % set seed
 rng('default')
-gamma_9 = get_coef_gamma(M, sigma, j_min, j_max, Npix, C, nu);
-[vec_field_curl_9, vec_field_div_9, vec_field_sum_9,...
-    mag_curl_grid_9, mag_div_grid_9, mag_sum_grid_9] = get_vec_field(N, N_grid, gamma_9,...
+gamma = get_coef_gamma(M, sigma, j_min, j_max, Npix, C, nu);
+[vec_field_curl, vec_field_div, vec_field_sum,...
+    mag_curl_grid, mag_div_grid, mag_sum_grid] = get_vec_field(N, N_grid, gamma,...
     A_2_1, A_2_2, A_3_1, A_3_2, A_2_1_grid, A_2_2_grid, A_3_1_grid, A_3_2_grid,...
     theta_grid);
 
 % plot
-plot_curl_div(theta, phi, theta_grid, phi_grid, vec_field_curl_9, vec_field_div_9, vec_field_sum_9,...
-    mag_curl_grid_9, mag_div_grid_9, mag_sum_grid_9)
-
-% uncorrelated case
-C = [1, 0; 0, 1];
-% set seed
-rng('default')
-gamma_0 = get_coef_gamma(M, sigma, j_min, j_max, Npix, C, nu);
-[vec_field_curl_0, vec_field_div_0, vec_field_sum_0,...
-    mag_curl_grid_0, mag_div_grid_0, mag_sum_grid_0] = get_vec_field(N, N_grid, gamma_0,...
-    A_2_1, A_2_2, A_3_1, A_3_2, A_2_1_grid, A_2_2_grid, A_3_1_grid, A_3_2_grid,...
-    theta_grid);
-
-% plot
-plot_curl_div(theta, phi, theta_grid, phi_grid, vec_field_curl_0, vec_field_div_0, vec_field_sum_0,...
-    mag_curl_grid_0, mag_div_grid_0, mag_sum_grid_0)
+plot_curl_div(theta, phi, theta_grid, phi_grid, vec_field_curl, vec_field_div, vec_field_sum,...
+    mag_curl_grid, mag_div_grid, mag_sum_grid)
