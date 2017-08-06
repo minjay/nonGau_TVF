641faddc30152b5a122b2bf8abe351eb1b95a61f
minjay <mjfan@ucar.edu>
2017-08-06 13:42:42 -0700

    Simulate t-distributed and normally distributed fields together.

--- plot_vec_field_j3.m
+++ plot_vec_field_j3.m
@@ -37,14 +37,39 @@ sigma = B.^((-alpha/2)*(j_min:j_max));
 % highly correlated case
 C = [1, 0.5; 0.5, 1];
 nu = 2.5;
+
+% sanity checking
+[gamma_t, gamma_g] = get_coef_gamma(1e7, sigma, j_min, j_max, 1e7, C, nu);
+% std(gamma_t) is more unstable
+std(gamma_t)
+std(gamma_g)
+sigma*sqrt(nu/(nu-2))
+
 % set seed
-rng('default')
-gamma = get_coef_gamma(M, sigma, j_min, j_max, Npix, C, nu);
-[vec_field_curl, vec_field_div, vec_field_sum,...
-    mag_curl_grid, mag_div_grid, mag_sum_grid] = get_vec_field(N, N_grid, gamma,...
+rng(1)
+[gamma_t, gamma_g] = get_coef_gamma(M, sigma, j_min, j_max, Npix, C, nu);
+
+
+[vec_field_curl_t, vec_field_div_t, vec_field_sum_t,...
+    mag_curl_grid_t, mag_div_grid_t, mag_sum_grid_t] = get_vec_field(N, N_grid, gamma_t,...
     A_2_1, A_2_2, A_3_1, A_3_2, A_2_1_grid, A_2_2_grid, A_3_1_grid, A_3_2_grid,...
     theta_grid);
 
-% plot
-plot_curl_div(theta, phi, theta_grid, phi_grid, vec_field_curl, vec_field_div, vec_field_sum,...
-    mag_curl_grid, mag_div_grid, mag_sum_grid)
+[vec_field_curl_g, vec_field_div_g, vec_field_sum_g,...
+    mag_curl_grid_g, mag_div_grid_g, mag_sum_grid_g] = get_vec_field(N, N_grid, gamma_g,...
+    A_2_1, A_2_2, A_3_1, A_3_2, A_2_1_grid, A_2_2_grid, A_3_1_grid, A_3_2_grid,...
+    theta_grid);
+
+theta = pi/2-theta;
+phi(phi>pi) = phi(phi>pi)-2*pi;
+[HX, HY] = sph2hammer(phi, theta);
+
+th = linspace(-pi/2,pi/2,101);
+lam = -pi+0*th;
+[xh,yh] = sph2hammer(lam,th);
+th2 = th;
+lam2 = pi+0*th;
+[xh2,yh2] = sph2hammer(lam2,th2);
+
+save('vec_field_j3.mat', 'HX', 'HY', 'vec_field_sum_t', 'vec_field_sum_g',...
+    'xh', 'yh', 'xh2', 'yh2')
